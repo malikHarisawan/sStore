71ef96a1ae16b62222767d302092a22b5449a2c9
malikHarisawan <malikharisawan06@gmail.com>
2024-06-27 16:57:42 +0500

    Initial commit

---
+++ store/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Order(models.Model):
+    PENDING   = "P"
+    COMPLETED = "C"
+    FAILED    = "F"
+
+    payment_choices = {
+        PENDING:"pending",
+        COMPLETED:"completed",
+        FAILED: "failed"
+    }
+
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=payment_choices , default=PENDING)
+
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    birth_date = models.DateField(null=True)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10 , decimal_places= 2)
+    inventory = models.IntegerField()
+
